83a1b51970ca71a16f1d4ccf654ebee56da9493a
ismail_e2_os <83674911+ismail9875@users.noreply.github.com>
2024-01-31 20:15:29 +0100

    Update dvbapi.sh

--- dvbapi.sh
+++ dvbapi.sh
@@ -2,14 +2,16 @@
 ## CMD=wget -O /etc/tuxbox/config/oscam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/oscam.dvbapi"
 ## CMD2=wget -O /etc/tuxbox/config/ncam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/ncam.dvbapi"
 #######################################
+wget https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/dvbapi.sh
 
 if [[ -f /etc/tuxbox/config/*.dvbapi ]]; then
-    rm -f *.dvbapi
-    get -O /etc/tuxbox/config/oscam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/oscam.dvbapi"
+    rm -f /etc/tuxbox/config/*.dvbapi
+    rm -f /etc/tuxbox/config/oscamicam/*.dvbapi
+    wget -O /etc/tuxbox/config/oscam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/oscam.dvbapi"
     wget -O /etc/tuxbox/config/ncam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/ncam.dvbapi"
 else
-    get -O /etc/tuxbox/config/oscam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/oscam.dvbapi"
+    wget -O /etc/tuxbox/config/oscam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/oscam.dvbapi"
     wget -O /etc/tuxbox/config/ncam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/ncam.dvbapi"
 fi 
-
+rm -f /tmp/*.dvbapi
 exit 0
