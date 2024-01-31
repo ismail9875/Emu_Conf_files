a143299bbc014291eb1bde6c6195403026cf9811
ismail_e2_os <83674911+ismail9875@users.noreply.github.com>
2024-01-31 20:17:38 +0100

    Update dvbapi.sh

--- dvbapi.sh
+++ dvbapi.sh
@@ -2,7 +2,6 @@
 ## CMD=wget -O /etc/tuxbox/config/oscam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/oscam.dvbapi"
 ## CMD2=wget -O /etc/tuxbox/config/ncam.dvbapi "https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/ncam.dvbapi"
 #######################################
-wget https://raw.githubusercontent.com/ismail9875/Emu_Conf_files/main/dvbapi.sh
 
 if [[ -f /etc/tuxbox/config/*.dvbapi ]]; then
     rm -f /etc/tuxbox/config/*.dvbapi
